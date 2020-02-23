ec988fdafcbc18fec1797f019387afbfd68eb6bf
tyler <tylerjohnson9893@gmail.com>
2020-02-23 11:48:52 -0600

    Set spanish as the default profile.

--- src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nSpanishService.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("ES")
+@Profile({"ES", "default"})
 @Service("I18nService")
 public class I18nSpanishService implements GreetingService {
     @Override
